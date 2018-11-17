62c4c5e087c42fda8b11156675a5153ea16f8966
naoya0082 <6ds3f7@gmail.com>
2018-11-17 16:25:08 +0900

    1~15までfizzbuzz完了。関数化。
    option + enter = エラー表示を確認

---
+++ FizzBuzz 1-100.py
@@ -0,0 +1,26 @@
+def fizzbuzz_convert(num):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(str(num))
+
+
+fizzbuzz_convert(1)
+fizzbuzz_convert(2)
+fizzbuzz_convert(3)
+fizzbuzz_convert(4)
+fizzbuzz_convert(5)
+fizzbuzz_convert(6)
+fizzbuzz_convert(7)
+fizzbuzz_convert(8)
+fizzbuzz_convert(9)
+fizzbuzz_convert(10)
+fizzbuzz_convert(11)
+fizzbuzz_convert(12)
+fizzbuzz_convert(13)
+fizzbuzz_convert(14)
+fizzbuzz_convert(15)
