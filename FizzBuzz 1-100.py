1ebe63084f98f1995e055a4f0dde90d13381c25f
naoya0082 <6ds3f7@gmail.com>
2018-11-17 16:34:56 +0900

    fizzbuzz変換の関数再デザイン。
    print -> return (convert(変換)の意味に合わせて)。
    「変換」に対して「出力」しているから

--- FizzBuzz 1-100.py
+++ FizzBuzz 1-100.py
@@ -1,26 +1,13 @@
 def fizzbuzz_convert(num):
     if num % 15 == 0:
-        print("FizzBuzz")
+        return "FizzBuzz"
     elif num % 3 == 0:
-        print("Fizz")
+        return "Fizz"
     elif num % 5 == 0:
-        print("Buzz")
+        return "Buzz"
     else:
-        print(str(num))
+        return str(num)
 
 
-fizzbuzz_convert(1)
-fizzbuzz_convert(2)
-fizzbuzz_convert(3)
-fizzbuzz_convert(4)
-fizzbuzz_convert(5)
-fizzbuzz_convert(6)
-fizzbuzz_convert(7)
-fizzbuzz_convert(8)
-fizzbuzz_convert(9)
-fizzbuzz_convert(10)
-fizzbuzz_convert(11)
-fizzbuzz_convert(12)
-fizzbuzz_convert(13)
-fizzbuzz_convert(14)
-fizzbuzz_convert(15)
+result = fizzbuzz_convert(1)
+print(result)
